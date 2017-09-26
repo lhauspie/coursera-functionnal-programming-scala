48e0900ede17d857a78a04f1d23edcdc0d33d350
Logan HAUSPIE <lhauspie@localhost.localdomain>
2017-09-26 23:33:45 +0200

    week 2

--- week-1.2/src/main/scala/recfun/Main.scala
+++ week-1.2/src/main/scala/recfun/Main.scala
@@ -11,17 +11,40 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || c == r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+
   /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def innerFunction(chars: List[Char], nbParentheses: Int): Int =
+      if (chars.isEmpty || nbParentheses < 0)
+        nbParentheses
+      else if (chars.head == '(')
+        innerFunction(chars.tail, nbParentheses + 1)
+      else if (chars.head == ')')
+        innerFunction(chars.tail, nbParentheses - 1)
+      else
+        innerFunction(chars.tail, nbParentheses)
+
+    innerFunction(chars, 0) == 0
   }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money < 0 || coins.isEmpty)
+      0
+    else if (money == 0 )
+      1
+    else
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
+}
